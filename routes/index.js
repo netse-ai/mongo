a3cfdec511fa4596ffdf017630c348ca8a0b3533
Trent Resh <tear728@gmail.com>
2019-02-12 15:29:13 -0800

    c

--- routes/index.js
+++ routes/index.js
@@ -58,6 +58,7 @@ router.get("/update-summoner/data=:data", (req, res) => {
         return res.json({success: true, code: 200, data: result})
         db.close();
       });
+      return res.json({success: false, code: 500, data: "Didnt make it"})
     });
 });
 
