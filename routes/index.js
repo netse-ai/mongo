dcada4d4b10e2ed28bc4b033afc37cdc57248ef9
Trent Resh <tear728@gmail.com>
2019-02-13 12:35:41 -0800

    c

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.post("/update-summoner/data", (req, res) => {
     MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
       if (err) throw err;
       var dbo = db.db("lolinsight");
-      var obj = req.params.data
+      var obj = req.params
       console.log(obj)
       var summoner = {
         $set:{
