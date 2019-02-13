6f6998c9e3d748c09fcb58545dd715c077630f2c
Trent Resh <tear728@gmail.com>
2019-02-13 14:28:02 -0800

    c

--- index.js
+++ index.js
@@ -6,35 +6,10 @@ var url = "mongodb://localhost:27017/mydb";
 
 const API_PORT = 3000;
 const app = express();
+/* GET home page. */
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(logger("dev"));
-
-function cleanStringify(object) {
-  if (object && typeof object === 'object') {
-      object = copyWithoutCircularReferences([object], object);
-  }
-  return JSON.stringify(object);
-
-  function copyWithoutCircularReferences(references, object) {
-      var cleanObject = {};
-      Object.keys(object).forEach(function(key) {
-          var value = object[key];
-          if (value && typeof value === 'object') {
-              if (references.indexOf(value) < 0) {
-                  references.push(value);
-                  cleanObject[key] = copyWithoutCircularReferences(references, value);
-                  references.pop();
-              } else {
-                  cleanObject[key] = '###_Circular_###';
-              }
-          } else if (typeof value !== 'function') {
-              cleanObject[key] = value;
-          }
-      });
-      return cleanObject;
-  }
-}
-
 
 app.get("/summoner/id=:id", (req, res) => {
     MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
@@ -54,10 +29,72 @@ app.get("/summoner/id=:id", (req, res) => {
 });
 
 app.post("/update-summoner", (req, res) => {
+  var obj = req.body
+  return res.json({success: true, code: 200, data: req.body})
 
-  res = cleanStringify(res)
-  console.log(res)
-  return res.json({success: true, code: 200, data: res})
+    // MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+    //   if (err) throw err;
+    //   var dbo = db.db("lolinsight");
+    //   var summoner = {
+    //     $set:{
+    //       "date": new Date()
+    //     },
+    //     $push:{
+    //       "matches": {
+    //         $each: obj.matches,
+    //       }
+    //     },
+    //   }
+    //   var insert = {
+    //     "id":obj.accountId,
+    //   }
+    //   dbo.collection("summoners").updateOne({id: obj.accountId}, summoner, {upsert:true}, (err, result) => {
+    //     if (err) throw err;
+    //     return res.json({success: true, code: 200, data: result})
+    //     db.close();
+    //   });
+    // });
 });
 
+// router.get("/insert-summoner/id=:id", (req, res) => {
+//     MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+//       if (err) throw err;
+//       var dbo = db.db("lolinsight");
+//       var obj = JSON.parse(req.params.id)
+//       var insert = {
+//         "id":obj,
+//       }
+//       dbo.collection('summoners').find(insert).toArray((err, result) => {
+//         if (err) throw err;
+//         console.log("Result: ", result, result.length)
+//         if (result.length == 0){
+//           dbo.collection("summoners").insertOne(insert, (err, result) => {
+//             if (err) throw err;
+//             db.close();
+//             return res.json({success: true, code: 200, data: result})
+//           });
+//         }
+//         else if (result.length > 0) {
+//           return res.json({success: true, code: 409, data: "Conflict Error. Summoner Already Exists"})
+//         }
+//         db.close()
+//       })
+//     });
+// });
+
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var mydb = db.db("mydb");
+//   console.log("Database created!");
+//   var myobj = { name: "Company Inc", address: "Highway 37" };
+//   mydb.collection("customers").insertOne(myobj, function(err, res) {
+//     if (err) throw err;
+//     console.log("1 document inserted");
+//     db.close();
+//   });
+// });
+
+
+// module.exports = router;
