1f186eb39226b032d903361e4c506361485fd314
Trent Resh <tear728@gmail.com>
2019-02-13 14:38:55 -0800

    c

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var url = "mongodb://localhost:27017/mydb";
 const API_PORT = 3000;
 const app = express();
 /* GET home page. */
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(logger("dev"));
 
 app.get("/summoner/id=:id", (req, res) => {
